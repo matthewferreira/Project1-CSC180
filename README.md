da9501bce4b2615c5ddf55c9c8e31f96e0acd701
matthewferreira <matthewferreira@csus.edu>
2021-09-27 16:24:41 -0700

    updated jupyter notebook project file, added init_data.py, changed readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Project1-CSC180
+After downloading the yelp dataset at https://www.yelp.com/dataset
+unzip the files into the same directory as the jupyter notebook file
+run the data_init script to create the 'review_stars.tsv' file
+then the jupyter notebook file should work
---
+++ init_data.py
@@ -0,0 +1,12 @@
+import json
+import csv
+import pandas as pd
+outfile = open("review_stars.tsv", 'w')
+sfile = csv.writer(outfile, delimiter ="\t", quoting=csv.QUOTE_MINIMAL)
+sfile.writerow(['business_id','stars', 'text'])
+with open('yelp_academic_dataset_review.json', encoding="utf-8") as f:     
+    for line in f:        
+        row = json.loads(line)        
+        # some special char must be encoded in 'utf-8'         
+        sfile.writerow([row['business_id'], row['stars'], (row['text']).encode('utf-8')])
+outfile.close()
